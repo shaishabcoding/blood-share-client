9a875941589199487bf206ce047b0e481b0a7960
shaishabcoding <myshaishab@gmail.com>
2024-08-02 12:21:37 +0600

    fix padding

--- src/pages/home/components/TopDonarAndRecentRequest/TopDonarAndRecentRequest.jsx
+++ src/pages/home/components/TopDonarAndRecentRequest/TopDonarAndRecentRequest.jsx
@@ -2,7 +2,7 @@ import RecentRequest from "../TopDonarAndRecentRequest/components/RecentRequest"
 import TopDonar from "../TopDonarAndRecentRequest/components/TopDonar";
 const TopDonarAndRecentRequest = () => {
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:my-24 md:gap-4 px-3 my-10 gap-10">
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:my-24 md:gap-4 px-3 my-6 gap-6">
       <div className="">
         <RecentRequest />
       </div>
